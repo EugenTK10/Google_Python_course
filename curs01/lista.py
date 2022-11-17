28573bb46956fdb077198c51eaa50354782c92a7
EugenTK10 <72226901+EugenTK10@users.noreply.github.com>
2022-11-17 19:47:04 +0100

    curs 1

---
+++ curs01/lista.py
@@ -0,0 +1,10 @@
+lista = []
+# print(type(lista))
+lista.append(3)
+lista.append(4)
+lista = [3, 4, 3, [1, '2', "element"], 'string']
+# print(lista[4:5])
+# print(lista[0:5:2])
+# print(lista[1:])
+print(lista[3][2])
+print(lista[3][2][2])
---
+++ curs01/seturi.py
@@ -0,0 +1,4 @@
+# seturi = {1,3,5}
+# print(seturi)
+lista = [1,2,3,4,5]
+print(set(lista))
